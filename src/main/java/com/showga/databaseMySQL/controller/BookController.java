fa0311dcc4aea4d903dfdcae895f52256d43d13d
ShowGa <showgacareer@gmail.com>
2024-11-06 16:23:02 +0800

    <Chapter 10 - 6> Book List Endpoint

--- src/main/java/com/showga/databaseMySQL/controller/BookController.java
+++ src/main/java/com/showga/databaseMySQL/controller/BookController.java
@@ -6,10 +6,10 @@ import com.showga.databaseMySQL.mappers.Mapper;
 import com.showga.databaseMySQL.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class BookController {
@@ -37,4 +37,17 @@ public class BookController {
         return new ResponseEntity<>(bookMapper.mapTo(createdBookEntity), HttpStatus.CREATED);
 
     }
+
+    @GetMapping("/books")
+    public List<BookDto> findAllBooks() {
+
+        // get all the books from the database
+        List<Book> books = bookService.findAll();
+
+        // convert the data into MapDto and send to client
+        return books.stream().map(bookMapper::mapTo).collect(Collectors.toList());
+
+    }
+
+
 }
--- src/main/java/com/showga/databaseMySQL/service/BookService.java
+++ src/main/java/com/showga/databaseMySQL/service/BookService.java
@@ -2,8 +2,11 @@ package com.showga.databaseMySQL.service;
 
 import com.showga.databaseMySQL.domain.entity.Book;
 
+import java.util.List;
+
 public interface BookService {
 
     Book createBook(String isbn, Book book);
 
+    List<Book> findAll();
 }
